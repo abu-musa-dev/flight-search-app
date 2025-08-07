89a21e47423697072d9f593f3347ea5406681d82
abumusa8 <abumusa0740@gmail.com>
2025-08-07 12:38:38 +0600

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-700">Welcome to Flight Search App</h1>
+    </div>
+  );
+}
+
+export default App;
