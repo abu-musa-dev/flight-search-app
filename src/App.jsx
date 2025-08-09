39367fb247984b933625dc497682979a721323f4
abumusa8 <abumusa0740@gmail.com>
2025-08-09 10:50:44 +0600

    add input fild

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Home from "./pages/Home";
 import FlightSearch from "./components/FlightSearch";
-import HeroSection from "./components/HeroSection";
+
 
 
 export default function App() {
   return <Home />;
   <FlightSearch></FlightSearch>;
-  <HeroSection></HeroSection>
+  
 }
