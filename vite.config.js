5c641b8391ea41053ea219ffaec1047b89fdaaa3
abumusa8 <abumusa0740@gmail.com>
2025-08-09 18:10:44 +0600

    add shared

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/flight-search-app/', // এখানে তোমার GitHub repo নাম বসাবে
+  base: '/', // Netlify এর জন্য বেস পাথ সরল রাখো
 });
